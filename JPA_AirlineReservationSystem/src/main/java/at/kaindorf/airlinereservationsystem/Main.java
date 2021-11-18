c9d100c09d4ee303be8c028e3c59578e06d43031
Shadow17402 <44363717+Shadow17402@users.noreply.github.com>
2021-11-18 21:18:01 +0100

    Airline done

--- JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airlinereservationsystem/Main.java
+++
@@ -1,4 +0,0 @@
-package at.kaindorf.airlinereservationsystem;
-
-public class Main {
-}
--- JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airlinereservationsystem/beans/Aircraft.java
+++ JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airlinereservationsystem/beans/Aircraft.java
@@ -1,39 +1,45 @@
 package at.kaindorf.airlinereservationsystem.beans;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Aircraft.getAllAircraftOfAirport", query = "SELECT a FROM Aircraft a JOIN a.airports ap WHERE ap.name = (:airport)"),
+        @NamedQuery(name = "Aircraft.getAllAircraftOfAirline", query = "SELECT a FROM Aircraft a WHERE a.airline.airlineName = (:airlineName)"),
+        //@NamedQuery(name = "Aircraft.getAllFlightIDFromAircraft", query = "SELECT f.flightId FROM Aircraft a JOIN a.flightList f")
+        @NamedQuery(name = "Aircraft.getAllAircraftOfType", query = "SELECT a FROM Aircraft a WHERE LOWER(a.aircraftType.typeName) LIKE LOWER(:typename)"),
+})
 public class Aircraft implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "aircraft_id")
     private Long aircraftId;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @NonNull
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @ToString.Exclude
     private Airline airline;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @NonNull
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "aircraft_type_id")
-    @ToString.Exclude
     private AircraftType aircraftType;
 
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name="aircraft_airport", joinColumns ={@JoinColumn(name="aircraft_id")}, inverseJoinColumns ={@JoinColumn(name="airport_id")})
     @ToString.Exclude
-    private List<Airport> airports;
+    private List<Airport> airports = new ArrayList<>();
 
-    @OneToMany(mappedBy = "aircraft", orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "aircraft", orphanRemoval = true)
     @ToString.Exclude
-    private List<Flight> flightList;
+    private List<Flight> flightList = new ArrayList<>();
 }
